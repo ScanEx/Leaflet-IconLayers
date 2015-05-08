466f142c4ddba24b5d640fb35dee7cb7b5495c90
zverev <alexander.zverev@gmail.com>
2015-05-08 17:34:26 +0300

    display layers list

---
+++ L.Control.IconLayers.js
@@ -0,0 +1,59 @@
++ function() {
+    function each(o, cb) {
+        for (var p in o) {
+            if (o.hasOwnProperty(p)) {
+                cb(o[p], p, o);
+            }
+        }
+    }
+
+    L.Control.IconLayers = L.Control.extend({
+        _createLayersElements: (function() {
+            function createLayerElement(layerObj) {
+                var el = L.DomUtil.create('div', 'leaflet-iconLayers-layer');
+                el.innerHTML = layerObj.title;
+                return el;
+            }
+
+            return function() {
+                each(this._layers, function(layer, id) {
+                    this._container.appendChild(createLayerElement(layer));
+                }.bind(this));
+            };
+        })(),
+        _attachEvents: (function() {
+            return function() {
+
+            };
+        })(),
+        // re-create control element and attach events
+        _render: function() {
+            this._createLayersElements();
+            this._attachEvents();
+        },
+        options: {
+            position: 'bottomleft'
+        },
+        initialize: function(layers, options) {
+            L.setOptions(this, options);
+            this.setLayers(layers);
+        },
+        onAdd: function(map) {
+            this._container = L.DomUtil.create('div', 'leaflet-iconLayers');
+            L.DomUtil.addClass(this._container, 'leaflet-iconLayers_' + this.options.position);
+            this._render();
+            return this._container;
+        },
+        setLayers: function(layers) {
+            this._layers = {};
+            layers.map(function(layer) {
+                this._layers[L.stamp(layer)] = layer;
+            }.bind(this));
+            this._container && this._render();
+        },
+        setActiveLayer: function(layer) {
+            this._activeLayerId = L.stamp(layer);
+            this._container && this._render();
+        }
+    });
+}();
