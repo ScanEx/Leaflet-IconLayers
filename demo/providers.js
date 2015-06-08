c13c9d89b8550e3ecdea316175424ce89d93d752
zverev <alexander.zverev@gmail.com>
2015-06-08 12:58:11 +0300

    add some providers

---
+++ demo/providers.js
@@ -0,0 +1,110 @@
+var providers = {};
+
+providers['OpenStreetMap_Mapnik'] = {
+    title: 'osm',
+    icon: 'icons/openstreetmap_mapnik.png',
+    layer: L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    })
+};
+
+providers['OpenStreetMap_BlackAndWhite'] = {
+    title: 'osm bw',
+    icon: 'icons/openstreetmap_blackandwhite.png',
+    layer: L.tileLayer('http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', {
+        maxZoom: 18,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    })
+};
+
+providers['OpenStreetMap_DE'] = {
+    title: 'osm de',
+    icon: 'icons/openstreetmap_de.png',
+    layer: L.tileLayer('http://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png', {
+        maxZoom: 18,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    })
+}
+
+providers['Stamen_Toner'] = {
+    title: 'toner',
+    icon: 'icons/stamen_toner.png',
+    layer: L.tileLayer('http://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}.png', {
+        attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+        subdomains: 'abcd',
+        minZoom: 0,
+        maxZoom: 20,
+        ext: 'png'
+    })
+};
+
+providers['Esri_WorldTerrain'] = {
+    title: 'esri terrain',
+    icon: 'icons/esri_worldterrain.png',
+    layer: L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Terrain_Base/MapServer/tile/{z}/{y}/{x}', {
+        attribution: 'Tiles &copy; Esri &mdash; Source: USGS, Esri, TANA, DeLorme, and NPS',
+        maxZoom: 13
+    })
+};
+
+providers['Esri_OceanBasemap'] = {
+    title: 'esri ocean',
+    icon: 'icons/esri_oceanbasemap.png',
+    layer: L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/Ocean_Basemap/MapServer/tile/{z}/{y}/{x}', {
+        attribution: 'Tiles &copy; Esri &mdash; Sources: GEBCO, NOAA, CHS, OSU, UNH, CSUMB, National Geographic, DeLorme, NAVTEQ, and Esri',
+        maxZoom: 13
+    })
+};
+
+providers['HERE_normalDay'] = {
+    title: 'normalday',
+    icon: 'icons/here_normalday.png',
+    layer: L.tileLayer('http://{s}.{base}.maps.cit.api.here.com/maptile/2.1/maptile/{mapID}/normal.day/{z}/{x}/{y}/256/png8?app_id={app_id}&app_code={app_code}', {
+        attribution: 'Map &copy; 1987-2014 <a href="http://developer.here.com">HERE</a>',
+        subdomains: '1234',
+        mapID: 'newest',
+        app_id: 'Y8m9dK2brESDPGJPdrvs',
+        app_code: 'dq2MYIvjAotR8tHvY8Q_Dg',
+        base: 'base',
+        maxZoom: 20
+    })
+};
+
+providers['HERE_normalDayGrey'] = {
+    title: 'normalday grey',
+    icon: 'icons/here_normaldaygrey.png',
+    layer: L.tileLayer('http://{s}.{base}.maps.cit.api.here.com/maptile/2.1/maptile/{mapID}/normal.day.grey/{z}/{x}/{y}/256/png8?app_id={app_id}&app_code={app_code}', {
+        attribution: 'Map &copy; 1987-2014 <a href="http://developer.here.com">HERE</a>',
+        subdomains: '1234',
+        mapID: 'newest',
+        app_id: 'Y8m9dK2brESDPGJPdrvs',
+        app_code: 'dq2MYIvjAotR8tHvY8Q_Dg',
+        base: 'base',
+        maxZoom: 20
+    })
+};
+
+providers['HERE_satelliteDay'] = {
+    title: 'satellite',
+    icon: 'icons/here_satelliteday.png',
+    layer: L.tileLayer('http://{s}.{base}.maps.cit.api.here.com/maptile/2.1/maptile/{mapID}/satellite.day/{z}/{x}/{y}/256/png8?app_id={app_id}&app_code={app_code}', {
+        attribution: 'Map &copy; 1987-2014 <a href="http://developer.here.com">HERE</a>',
+        subdomains: '1234',
+        mapID: 'newest',
+        app_id: 'Y8m9dK2brESDPGJPdrvs',
+        app_code: 'dq2MYIvjAotR8tHvY8Q_Dg',
+        base: 'aerial',
+        maxZoom: 20
+    })
+};
+
+providers['CartoDB_Positron'] = {
+    title: 'positron',
+    icon: 'icons/cartodb_positron.png',
+    layer: L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://cartodb.com/attributions">CartoDB</a>',
+        subdomains: 'abcd',
+        maxZoom: 19
+    })
+};
